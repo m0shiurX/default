1de796d3d1885bf43f18f432b42a4e47469f5da7
Moshiur Rahman <unimrgm@gmail.com>
2024-03-05 18:02:06 +0600

    initial layout

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import './style.css';
+import 'video.js/dist/video-js.css';
+
+import videojs from 'video.js';
+
+let options = {
+	height: '360',
+	width: '540',
+	fluid: false,
+	autoplay: true,
+	controls: false,
+	preload: 'auto',
+	controlBar: {
+		volumePanel: { inline: false },
+	},
+};
+const player = videojs('vid_player', options);
